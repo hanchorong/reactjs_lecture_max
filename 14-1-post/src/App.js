364196eecc14388374f208682e6220537a8ce29e
hanchorong <hancrong@gmail.com>
2023-05-16 11:48:35 +0900

    post

--- 14-1-post/src/App.js
+++ 14-1-post/src/App.js
@@ -14,23 +14,26 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-62e2b-default-rtdb.firebaseio.com/movies.json"
+        "https://react-http-f8cdf-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
-      console.log(data);
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      console.log("data", data);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -43,7 +46,7 @@ function App() {
 
   async function addMovieHandler(movie) {
     const response = await fetch(
-      "https://react-http-62e2b-default-rtdb.firebaseio.com/movies.json",
+      "https://react-http-f8cdf-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
       {
         method: "POST",
         body: JSON.stringify(movie),
