36c2013a5e1cd8d14b2b49ef0b2e39a04d9c1099
hanchorong <hancrong@gmail.com>
2023-06-14 22:05:16 +0900

    auth

--- 18.redux/src/store/index.js
+++ 18.redux/src/store/index.js
@@ -6,14 +6,14 @@ import { createSlice, configureStore } from "@reduxjs/toolkit";
 // export const DECREMENT = "decrement";
 // export const TOGGLE = "toggle";
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -62,10 +62,31 @@ const counterSlice = createSlice({
 //   return state;
 // };
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
