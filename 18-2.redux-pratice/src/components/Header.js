85a8a491e80c0a56465469490cdf16a216bf9ca1
hanchorong <hancrong@gmail.com>
2023-06-30 19:22:52 +0900

    redux

---
+++ 18-2.redux-pratice/src/components/Header.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { authActions } from "../store";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  const navHandler = () => {
+    dispatch(authActions.logout());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+
+      {auth ? (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={navHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
+    </header>
+  );
+};
+
+export default Header;
